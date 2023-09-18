e09cec88eed07a7557cf71c710d8676b88760384
Josue Ferreyra <josueferreyra8824@gmail.com>
2023-09-18 20:34:36 -0300

    ADDED: Skills section

---
+++ src/assets/icons/icons.tsx
@@ -0,0 +1,77 @@
+export const ArrowDownIcon = ({ classList } : { classList: string | undefined}) => (
+  <svg className={classList} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M12 5l0 14"></path>
+    <path d="M18 13l-6 6"></path>
+    <path d="M6 13l6 6"></path>
+  </svg>
+)
+export const ReactIcon = ({ classList } : { classList: string | undefined}) => (
+  <svg className={classList} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M6.306 8.711c-2.602 .723 -4.306 1.926 -4.306 3.289c0 2.21 4.477 4 10 4c.773 0 1.526 -.035 2.248 -.102"></path>
+    <path d="M17.692 15.289c2.603 -.722 4.308 -1.926 4.308 -3.289c0 -2.21 -4.477 -4 -10 -4c-.773 0 -1.526 .035 -2.25 .102"></path>
+    <path d="M6.305 15.287c-.676 2.615 -.485 4.693 .695 5.373c1.913 1.105 5.703 -1.877 8.464 -6.66c.387 -.67 .733 -1.339 1.036 -2"></path>
+    <path d="M17.694 8.716c.677 -2.616 .487 -4.696 -.694 -5.376c-1.913 -1.105 -5.703 1.877 -8.464 6.66c-.387 .67 -.733 1.34 -1.037 2"></path>
+    <path d="M12 5.424c-1.925 -1.892 -3.82 -2.766 -5 -2.084c-1.913 1.104 -1.226 5.877 1.536 10.66c.386 .67 .793 1.304 1.212 1.896"></path>
+    <path d="M12 18.574c1.926 1.893 3.821 2.768 5 2.086c1.913 -1.104 1.226 -5.877 -1.536 -10.66c-.375 -.65 -.78 -1.283 -1.212 -1.897"></path>
+    <path d="M11.5 12.866a1 1 0 1 0 1 -1.732a1 1 0 0 0 -1 1.732z"></path>
+  </svg>
+)
+export const NextIcon = ({ classList } : { classList: string | undefined}) => (
+  <svg className={classList} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M9 15v-6l7.745 10.65a9 9 0 1 1 2.255 -1.993"></path>
+    <path d="M15 12v-3"></path>
+  </svg>
+)
+export const TypescriptIcon = ({ classList } : { classList: string | undefined}) => (
+  <svg className={classList} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M15 17.5c.32 .32 .754 .5 1.207 .5h.543c.69 0 1.25 -.56 1.25 -1.25v-.25a1.5 1.5 0 0 0 -1.5 -1.5a1.5 1.5 0 0 1 -1.5 -1.5v-.25c0 -.69 .56 -1.25 1.25 -1.25h.543c.453 0 .887 .18 1.207 .5"></path>
+    <path d="M9 12h4"></path>
+    <path d="M11 12v6"></path>
+    <path d="M21 19v-14a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2 -2z"></path>
+  </svg>
+)
+export const CodeIcon = ({ classList } : { classList: string | undefined}) => (
+  <svg className={classList} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M7 8l-4 4l4 4"></path>
+    <path d="M17 8l4 4l-4 4"></path>
+    <path d="M14 4l-4 16"></path>
+  </svg>
+)
+export const GitIcon = ({ classList } : { classList: string | undefined}) => (
+  <svg className={classList} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M16 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+    <path d="M12 8m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+    <path d="M12 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+    <path d="M12 15v-6"></path>
+    <path d="M15 11l-2 -2"></path>
+    <path d="M11 7l-1.9 -1.9"></path>
+    <path d="M13.446 2.6l7.955 7.954a2.045 2.045 0 0 1 0 2.892l-7.955 7.955a2.045 2.045 0 0 1 -2.892 0l-7.955 -7.955a2.045 2.045 0 0 1 0 -2.892l7.955 -7.955a2.045 2.045 0 0 1 2.892 0z"></path>
+  </svg>
+)
+export const CssIcon = ({ classList } : { classList: string | undefined}) => (
+  <svg className={classList} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z"></path>
+    <path d="M8.5 8h7l-4.5 4h4l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5"></path>
+  </svg>
+)
+export const BootstrapIcon = ({ classList } : { classList: string | undefined}) => (
+  <svg className={classList} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+   <path d="M2 12a2 2 0 0 0 2 -2v-4a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v4a2 2 0 0 0 2 2"></path>
+   <path d="M2 12a2 2 0 0 1 2 2v4a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-4a2 2 0 0 1 2 -2"></path>
+   <path d="M9 16v-8h3.5a2 2 0 1 1 0 4h-3.5h4a2 2 0 1 1 0 4h-4z"></path>
+</svg>
+)
+export const TailwindIcon = ({ classList } : { classList: string | undefined}) => (
+  <svg className={classList} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968zm-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968z"></path>
+  </svg>
+)
---
+++ src/componets/Skills.tsx
@@ -0,0 +1,28 @@
+import { techs as skills } from '../data.json'
+import { iconsMap } from '../assets/icons/iconsMap'
+
+export function Skills() {
+  return (
+    <section id="skills" className="snap-start">
+      <div className="max-w-4xl m-auto px-3">
+        <div className="w-fit">
+          <h2 className='text-5xl font-black pt-3'>
+            Skills
+          </h2>
+          <div className="h-3 w-full bg-amber-500 -mt-3"></div>
+        </div>
+
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(6rem,1fr))] gap-3 mt-6">
+          {
+            skills.map(skill => (
+              <div key={skill.name} className={`flex flex-col items-center p-2 rounded-lg ${skill.color} ${skill.bg}`}>
+                {iconsMap[skill.name as keyof typeof iconsMap]({classList: "h-8 w-8"})}
+                <h3 className="text-sm font-semibold capitalize">{skill.name}</h3>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </section>
+  )
+}

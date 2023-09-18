b171a4882ce54857bab1f0715f23e3ae88c2ab43
Josue Ferreyra <josueferreyra8824@gmail.com>
2023-09-18 20:32:55 -0300

    CREATED: AboutMe section

---
+++ src/componets/AboutMe.tsx
@@ -0,0 +1,23 @@
+export function AboutMe() {
+ 
+  return (
+    <section className="snap-start">
+      <div className="max-w-4xl m-auto px-3">
+        <div className="w-fit">
+          <h2 className='text-5xl font-black pt-3'>
+            About Me
+          </h2>
+          <div className="h-3 w-full bg-sky-500 -mt-3"></div>
+        </div>
+        <div>
+          <p className="text-lg mt-6">
+            I'm Josué Ferreyra, a self-taught frontend developer with a <span className="text-sky-500 font-semibold">background in Software Engineering</span> studies and a <a className="text-sky-500 font-semibold hover:underline" href="https://www.efset.org/cert/df8MP5" target="_blank" rel="noopener noreferrer">C2 certification in English </a>proficiency. My journey in web development began through independent learning, where I have become proficient in <span className="text-sky-500 font-semibold">frontend development</span>.
+          </p>
+          <p className="text-lg mt-2"> 
+            Beyond technical skills, I bring a passion for <span className="text-sky-500 font-semibold">collaboration</span>, a natural inclination to <span className="text-sky-500 font-semibold">help others</span>, and a deep commitment to <span className="text-sky-500 font-semibold">personal and professional growth</span>. I'm eager to contribute my <span className="text-sky-500 font-semibold">motivation, creativity, and fresh ideas</span> to an innovative team that values mentorship and good vibes.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}

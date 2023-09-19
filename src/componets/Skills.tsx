83a9934412821c872d95d55a9171582d6509a8ea
Josue Ferreyra <josueferreyra8824@gmail.com>
2023-09-19 20:22:35 -0300

    TEMP FIX: Technologies colors

--- src/componets/Skills.tsx
+++ src/componets/Skills.tsx
@@ -1,6 +1,27 @@
 import { techs as skills } from '../data.json'
 import { iconsMap } from '../assets/icons/iconsMap'
 
+/*
+  Pequeño "FIX" para que los colores de las skills puedan añadirse de forma dinámica debido a que Tailwind NO se ejecuta en el cliente, al hacer el fetch de las propiedades "bg" y "color" se añaden a la clase, pero al no haber sido previamente mencionadas en el código, Tailwind no tiene sus valores cargados. Es por eso que en un comentario cargamos todos los colores que vamos a usar desde DATA.json
+
+  De todas formas es una solución bastante pobre y no me gusta, pero no he visto otra forma de hacerlo, ya que, sino habría que poner el DATA.json como un objeto dentro del componente, cosa que quiero evitar. En un futuro tal vez encuentro una solución más elegante. Por ahora esto es lo que funciona.
+
+  "color": "text-cyan-800"
+  "bg": "bg-cyan-400"
+  "color": "text-white"
+  "bg": "bg-black"
+  "color": "text-blue-900"
+  "bg": "bg-blue-400"
+  "color": "text-orange-900"
+  "bg": "bg-orange-400"
+  "color": "text-sky-900"
+  "bg": "bg-sky-400"
+  "color": "text-violet-900"
+  "bg": "bg-violet-400"
+  "color": "text-teal-900"
+  "bg": "bg-teal-400"
+*/ 
+
 export function Skills() {
   return (
     <section id="skills" className="snap-start">

8fb016913903409090ed11bc785c0c8dbb598f92
Josue Ferreyra <josueferreyra8824@gmail.com>
2023-09-20 10:44:06 -0300

    REFACTOR: Tech data && FIXED: Tech colors now should display correctly

---
+++ src/utils/techs.ts
@@ -0,0 +1,46 @@
+import { BootstrapIcon, CssIcon, GitIcon, NextIcon, ReactIcon, TailwindIcon, TypescriptIcon } from "../assets/icons/icons";
+
+export const techs = [
+  {
+    name: "React",
+    icon: ReactIcon,
+    bg: "bg-cyan-400",
+    color: "text-cyan-900"
+  },
+  {
+    name: "Next.js",
+    icon: NextIcon,
+    bg: "bg-zinc-950",
+    color: "text-white"
+  },
+  {
+    name: "Typescript",
+    icon: TypescriptIcon,
+    bg: "bg-blue-400",
+    color: "text-blue-900"
+  },
+  {
+    name: "Git",
+    icon: GitIcon,
+    bg: "bg-orange-500",
+    color: "text-orange-900"
+  },
+  {
+    name: "CSS",
+    icon: CssIcon,
+    bg: "bg-sky-400",
+    color: "text-sky-900"
+  },
+  {
+    name: "Bootstrap",
+    icon: BootstrapIcon,
+    bg: "bg-violet-400",
+    color: "text-violet-900"
+  },
+  {
+    name: "Tailwind",
+    icon: TailwindIcon,
+    bg: "bg-teal-400",
+    color: "text-teal-900"
+  }
+]
